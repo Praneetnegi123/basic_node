15ac098132831e12f684b9a744adb96c15a20487
Praneetnegi123 <47327874+Praneetnegi123@users.noreply.github.com>
2025-05-03 22:18:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app= express();
+
+
+app.use(express.json());
+
+const PORT=4008
+
+
+// Get request
+app.get('/getRequests', (req, res) => {
+    res.send('All Products Here');
+}
+);
+
+// Post request
+app.post('/postRequest', (req, res) => {
+    // Simulate product creation
+    console.log("req.body=>>>",req.body)
+    // insert into database
+    // const product = new Product(req.body);
+    res.send('Product Created');
+}
+);
+
+// Put request
+app.put('/putRequest/:id', (req, res) => {
+    console.log("req.params.id=>>>",req.params.id)
+    console.log("req.body=>>>",req.body)
+    // Simulate product update
+    // const product = await Product.findByIdAndUpdate(req.params.id    , req.body  );
+    res.send('Product Updated');
+}
+);
+
+// Delete request
+app.delete('/deleteRequest/:productId', (req, res) => {
+    console.log("req.params.productId=>>>",req.params.productId)
+    // Simulate product deletion
+    // const product = await Product.findByIdAndDelete(req.params.productId);
+    res.send('Product Deleted');
+}
+);  
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);   
+})
